88da84fb51b7b233094dec74b1bb3ae3d8caed22
carevalom <cristians.arevalom@gmail.com>
2021-12-08 23:03:28 -0500

    Solucion Bug relacion Rol y user

--- auth_ms/AuthApp/serializers/userSerializer.py
+++ auth_ms/AuthApp/serializers/userSerializer.py
@@ -5,20 +5,20 @@ from AuthApp.models.rol import Rol
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password','name','last_name','cedula','create_time','email','rol']
+        fields = ['id','username', 'password','name','last_name','cedula','create_time','email','rol']
  
-    def to_representation (self, obj):
-        user = User.objects.get(id = obj.id)
-        rol = Rol.objects.get(id = user.id) ##trae el rol asociada al usuario, en donde el id del rol sea igual al campo user.id
+    def to_representation (self, obj): #obj es usuario
+        userData = User.objects.get(id=obj.id)
+        rolData = Rol.objects.get(id=obj.rol_id) ##trae el campo rol en donde el id de rol sea igual al del objeto de usuario atributo rol_id
         return{
-            'username':user.username,
-            'name':user.name,
-            'last_name':user.last_name,
-            'cedula':user.cedula,
-            'create_time':user.create_time,
-            'email':user.email,
+            'username':userData.username,
+            'name':userData.name,
+            'last_name':userData.last_name,
+            'cedula':userData.cedula,
+            'create_time':userData.create_time,
+            'email':userData.email,
             'rol':{
-               'name':rol.name,
-                'description':rol.description,
+               'name':rolData.name,
+                'description':rolData.description,
             }
         }
